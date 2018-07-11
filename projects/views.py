588a1db2961a0e1b7a1a9a2f1fd96a07acccc37b
Blankphrase <ckasera6486@gmail.com>
2018-07-11 16:57:36 +0300

    creation of projects view function to display projects

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse,Http404
+from .models import Project
 
 # Create your views here.
+def projects(request):
+    projects = Project.all_projects()
+
+    return render(request, 'project.html', {"projects": projects})
+    
