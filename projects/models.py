e62162f89eb30b44c9ae6b503d2be600b2ff42d4
Blankphrase <ckasera6486@gmail.com>
2018-07-11 16:50:57 +0300

    creation of projects model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    live_link = models.CharField(max_length=250)
+    github_link = models.CharField(max_length = 250)
+    screenshot = models.ImageField(upload_to = '/')
+
+    @classmethod
+    def all_projects(cls):
+        projects = cls.objects.all()
+        return projects
